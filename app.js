4b908976ee95a2703ee4fffb1ca63ce0b78d1343
wardaharshad <54935867+wardaharshad@users.noreply.github.com>
2020-06-14 17:03:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,113 @@
+// chapter 1 task 1
+alert("Hello!!");
+
+// chapter 1 task 2
+alert("Error! Please Enter a valid Password.");
+
+// chapter 1 task 3
+alert("Welcome to js Land... \nHappy coding");
+
+// chapter 1 task 4
+alert("Welcome to js Land...");
+alert("Happy coding \nPrevent this page from creating additional dialogues.");
+
+// chapter 2 task 1
+var username;
+
+// chapter 2 task 2
+var myName = "Abdullah Arshad";
+
+// chapter 2 task 3
+var message;
+message = "Hello World";
+alert(message);
+
+// chapter 2 task 4
+var nam = "John Doe";
+var age = " 15 years old";
+var qualification = "certified mobile application development";
+alert(nam);
+alert(age);
+alert(qualification);
+
+// chapter 2 task 5
+// var a = PIZZA;
+// var i;
+// var j;
+// var b;
+// for (j = 0; j <= a.length; j++){
+// 	for(i < a.length; i >= 0; i--){
+//     	b = a.splice(0,i);
+//     }
+//     "\n"
+// }
+// alert(b);
+
+// chapter 2 task 6
+var email = "abdqureshi41@gmail.com";
+alert("my email address is " + email);
+
+// chapter 2 task 7
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book " + book);
+
+// chapter 2 task 8
+document.write("Yah! I can write HTML content through JavaScript \n");
+
+// chapter 2 task 9
+var d = "“▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬”"
+alert(d);
+
+// chapter 3 task 1
+var aged;
+aged = 21;
+alert("I am " + aged + " years old.");
+
+// chapter 3 task 2
+var vsit;
+vsit = 21;
+alert("You have visited this site " + vsit + " times");
+
+// chapter 3 task 3
+var birthyear;
+birthyear = 1999;
+document.write("My birthyear is " + birthyear + " \n data type of my declared variable is number \n");
+
+// chapter 3 task 4
+var visitorName;
+var productt;
+var quantity;
+visitorName = "John Doe";
+productt = "T-Shirts";
+quantity = 5;
+document.write(visitorName + " ordered " + quantity + " " + productt + " on xyz clothing store.")
+
+// chapter 4 task 1
+var p; var q; var r;
+
+// chapter 4 task 2
+// Legal
+var myXfd;
+var mr12;
+var ssss;
+var dkl;
+var sld;
+
+// illegal
+var 1wri;
+var 4rr;
+var 8uhj;
+var 456;
+var 6t;
+
+// chapter 5 task 1
+var one = +prompt("number 1");
+var two = +prompt("number 2");
+alert(" sum of " + one + " and " + two + " is " + one + two);
+
+// chapter 5 task 2
+alert(" difference of " + one + " and " + two + " is " + one - two);
+alert(" multiplication of " + one + " and " + two + " is " + one * two);
+alert(" division of " + one + " and " + two + " is " + one / two);
+
+// chapter 5 task 1
